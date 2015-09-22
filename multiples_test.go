787fb3189f00f8aa6dc0f84a46b780fe9e7973bf
Michael Roark <m@mroark.net>
2015-09-21 22:25:51 -0700

    Add test file and rename prob1 file

---
+++ multiples_test.go
@@ -0,0 +1,14 @@
+package projeuler_test
+
+import (
+    "testing"
+    "fmt"
+)
+
+
+func TestShouldSolveVerbatim(t *testing.T) {
+    // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+    // The sum of these multiples is 23.
+    // FIND THE SUM OF ALL THE MULTIPLES OF 3 OR 5 BELOW 1000.
+    fmt.Println("poop")
+}
