f3afadde644254d95d6d55e8326725bb7c461b75
Michael Roark <m@mroark.net>
2015-09-21 23:13:41 -0700

    Add tests for problem one

--- multiples_test.go
+++ multiples_test.go
@@ -2,13 +2,64 @@ package euler
 
 import (
     "testing"
-    "fmt"
 )
 
 
 func TestShouldSolveVerbatim(t *testing.T) {
     // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
     // The sum of these multiples is 23.
+
     // FIND THE SUM OF ALL THE MULTIPLES OF 3 OR 5 BELOW 1000.
-    fmt.Println("poop")
+    n, e := findIntsLessThanAndDivisibleBy(1000, 3, 5)
+    r := 233168
+    if n != r {
+        t.Errorf("Result should equal %d but was %d", r, n)
+    }
+    if e != nil {
+        t.Errorf("Error raised when there should be none.")
+    }
+}
+
+func TestShouldSetErrorForArsOutOfBounds(t * testing.T) {
+    _, e := findIntsLessThanAndDivisibleBy(1000, -3, 5)
+    if e == nil {
+        t.Errorf("No error was set when a multiple was set to a negative.")
+    }
+
+    _, e = findIntsLessThanAndDivisibleBy(-1000, 3, 5)
+    if e == nil {
+        t.Errorf("No error was set when the cap was set to a negative.")
+    }
+
+    _, e = findIntsLessThanAndDivisibleBy(100, 101, 5)
+    if e == nil {
+        t.Errorf("No error was set when a multiple was greater than the cap.")
+    }
+}
+
+
+func TestShouldFindOccurencesOfMultiples(t * testing.T) {
+    n, _ := findIntsLessThanAndDivisibleBy(10, 3)
+    r := 18
+    if n != r {
+        t.Errorf("Result should equal %d but was %d", r, n)
+    }
+
+    n, _ = findIntsLessThanAndDivisibleBy(100, 9)
+    r = 594
+    if n != r {
+        t.Errorf("Result should equal %d but was %d", r, n)
+    }
+
+    n, _ = findIntsLessThanAndDivisibleBy(99, 9)
+    r = 495
+    if n != r {
+        t.Errorf("Result should equal %d but was %d", r, n)
+    }
+
+    n, _ = findIntsLessThanAndDivisibleBy(100, 9, 4, 23)
+    r = 1824
+    if n != r {
+        t.Errorf("Result should equal %d but was %d", r, n)
+    }
 }
